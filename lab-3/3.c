d7412ec735dcadcc5f079c7dfa01e0e36b421a6f
Tanim <tanvirhasantanim06@gmail.com>
2023-04-15 23:40:18 +0600

    Complete lab 2 3 4

--- lab-3/3.c
+++ lab-3/3.c
@@ -4,10 +4,12 @@ int main() {
    int test_number;
    printf("Enter a test number: ");
    scanf("%d", &test_number);
-    if(test_number % 5 == 0 && test_number % 7 == 0 && (test_number % 11 != 0)){
-        
+    if(test_number % 5 == 0 && test_number % 7 == 0 && test_number % 11 != 0){
+        printf("test the number %d is multiple of 5 or not, divisible by 7 but not by eleven.\n" , test_number);
+        printf("Condition Passed\n");
     }else{
-
+        printf("test the number is %d is not multiple of 5 or not, divisible by 7 but not by eleven.\n" , test_number);
+        printf("Condition Failed\n");
     }
 
    return 0;
